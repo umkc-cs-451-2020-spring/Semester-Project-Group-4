d15da3f66c5480b1acc506ab788057042c602716
Harrison R. Lara <harrisonlara97@gmail.com>
2020-02-22 23:18:22 -0600

    add global theme provider

--- Commerce-Project/client/src/app/index.tsx
+++ Commerce-Project/client/src/app/index.tsx
@@ -4,17 +4,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginForm from '../Pages/login/login';
 import RegistrationForm from '../Pages/registration/RegistrationForm';
 import DashBoard from '../Pages/dashboard/dashboard';
+import { ThemeProvider } from '@material-ui/core';
+import { theme } from '../components/theme';
 
 const App = () => {
 
   return (
     <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={LoginForm} />
-        <Route path="/register" exact component={RegistrationForm} />
-        <Route path="/dashboard" exact component={DashBoard} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={LoginForm} />
+          <Route path="/register" exact component={RegistrationForm} />
+          <Route path="/dashboard" exact component={DashBoard} />
+        </Switch>
+      </ThemeProvider>
     </Router>
   )
 }
---
+++ Commerce-Project/client/src/components/theme.tsx
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  }
+});
