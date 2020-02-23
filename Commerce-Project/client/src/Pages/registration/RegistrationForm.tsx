36afbddc6eeda6b57f3c99ee403925305f75b69c
Harrison R. Lara <harrisonlara97@gmail.com>
2020-02-22 21:18:49 -0600

    add validators to form

--- Commerce-Project/client/src/Pages/registration/RegistrationForm.tsx
+++ Commerce-Project/client/src/Pages/registration/RegistrationForm.tsx
@@ -17,6 +17,46 @@ const useStyles = makeStyles(() =>
   }),
 );
 
+const validate = (values: any) => {
+  const errors = {} as any;
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 50) {
+    errors.firstName = 'Must be 50 characters or less';
+  }
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 50) {
+    errors.lastName = 'Must be 50 characters or less';
+  }
+  if (!values.username) {
+    errors.username = 'Required';
+  } else if (values.username.length > 20) {
+    errors.username = 'Must be 20 characters or less';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.phone) {
+    errors.phone = 'Required';
+  } else if (!/\/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{10})$/i.test(values.phone)) {
+    errors.phone = 'Invalid phone number';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (!/\/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/i.test(values.password)) {
+    errors.password = 'Invalid password. Must contain at least 8 characters, a special character, a number, one uppercase and lowercase letter';
+  }
+  if (!values.confirmPassword) {
+    errors.confirmPassword = 'Required';
+  } else if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+  return errors;
+};
+
 const RegistrationForm = () => {
   const { textFieldMargin, buttonMargin, gridMargin } = useStyles();
 
@@ -30,7 +70,8 @@ const RegistrationForm = () => {
       password: '',
       confirmPassword: ''
     },
-    onSubmit: values => {
+    validate,
+    onSubmit: (values: any) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
@@ -50,6 +91,9 @@ const RegistrationForm = () => {
               type="text"
               onChange={formik.handleChange}
               value={formik.values.firstName}
+              helperText={formik.errors.firstName && formik.touched.firstName ? formik.errors.firstName : null}
+              error={formik.touched.firstName && formik.errors.firstName ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
@@ -59,24 +103,33 @@ const RegistrationForm = () => {
               type="text"
               onChange={formik.handleChange}
               value={formik.values.lastName}
+              helperText={formik.errors.lastName && formik.touched.lastName ? formik.errors.lastName : null}
+              error={formik.touched.lastName && formik.errors.lastName ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
               label="Username"
-              id="Username"
-              name="Username"
+              id="username"
+              name="username"
               type="text"
               onChange={formik.handleChange}
               value={formik.values.username}
+              helperText={formik.errors.username && formik.touched.username ? formik.errors.username : null}
+              error={formik.touched.username && formik.errors.username ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
               label="Email"
-              id="Email"
-              name="Email"
+              id="email"
+              name="email"
               type="text"
               onChange={formik.handleChange}
               value={formik.values.email}
+              helperText={formik.errors.email && formik.touched.email ? formik.errors.email : null}
+              error={formik.touched.email && formik.errors.email ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
@@ -86,6 +139,9 @@ const RegistrationForm = () => {
               type="text"
               onChange={formik.handleChange}
               value={formik.values.phone}
+              helperText={formik.errors.phone && formik.touched.phone ? formik.errors.phone : null}
+              error={formik.touched.phone && formik.errors.phone ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
@@ -95,6 +151,9 @@ const RegistrationForm = () => {
               type="text"
               onChange={formik.handleChange}
               value={formik.values.password}
+              helperText={formik.errors.password && formik.touched.password ? formik.errors.password : null}
+              error={formik.touched.password && formik.errors.password ? true : false}
+              onBlur={formik.handleBlur}
             />
             <TextField
               className={textFieldMargin}
@@ -104,6 +163,9 @@ const RegistrationForm = () => {
               type="text"
               onChange={formik.handleChange}
               value={formik.values.confirmPassword}
+              helperText={formik.errors.confirmPassword && formik.touched.confirmPassword ? formik.errors.confirmPassword : null}
+              error={formik.touched.confirmPassword && formik.errors.confirmPassword ? true : false}
+              onBlur={formik.handleBlur}
             />
             <Button className={buttonMargin} type="submit" variant="contained" color="primary">Register</Button>
           </FormGroup>
