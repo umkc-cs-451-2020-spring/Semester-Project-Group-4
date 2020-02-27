8bd02d24529378e86f9723e02ebcd9280b391b1f
Harrison R. Lara <harrisonlara97@gmail.com>
2020-02-26 21:47:06 -0600

    init dashboard

--- Commerce-Project/client/src/Pages/dashboard/dashboard.tsx
+++ Commerce-Project/client/src/Pages/dashboard/dashboard.tsx
@@ -1,12 +1,73 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Divider, Paper, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Theme } from '../../components';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    paper: {
+      padding: '1rem',
+      height: '100%'
+    },
+    divider: {
+      marginTop: '2rem',
+      marginBottom: '2rem'
+    },
+    expansion: {
+      marginBottom: '2rem'
+    },
+    expansionHeader: {
+      flexBasis: '33.33%'
+    },
+    balance: {
+      color: Theme.palette.primary.main,
+      fontWeight: 700,
+      marginLeft: '.25rem',
+    }
+  }),
+);
 
 const DashBoard = () => {
+  const { paper, divider, expansion, expansionHeader, balance } = useStyles()
 
   return (
-    <Typography>
-      Dashboard Here.
-    </Typography>
+    <Paper elevation={0} className={paper}>
+      <Typography variant='h5'>Welcome back, Name</Typography>
+      <Divider className={divider} />
+      <ExpansionPanel className={expansion}>
+        <ExpansionPanelSummary id="panel1" expandIcon={<ExpandMoreIcon />}>
+          <Typography variant='h6' className={expansionHeader}>Checking Account</Typography>
+          <Typography variant='subtitle1'>Balance: </Typography>
+          <Typography variant='subtitle1' className={balance}>$1000</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          Summary Details
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+
+      <ExpansionPanel className={expansion}>
+        <ExpansionPanelSummary id="panel2" expandIcon={<ExpandMoreIcon />}>
+          <Typography variant='h6' className={expansionHeader}>Savings Account</Typography>
+          <Typography variant='subtitle1'>Balance: </Typography>
+          <Typography variant='subtitle1' className={balance}>$1000</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          Summary Details
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+
+      <ExpansionPanel className={expansion}>
+        <ExpansionPanelSummary id="panel3" expandIcon={<ExpandMoreIcon />}>
+          <Typography variant='h6' className={expansionHeader}>Money Market Account</Typography>
+          <Typography variant='subtitle1'>Balance:</Typography>
+          <Typography variant='subtitle1' className={balance}>$1000</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          Summary Details
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </Paper >
   )
 }
 
