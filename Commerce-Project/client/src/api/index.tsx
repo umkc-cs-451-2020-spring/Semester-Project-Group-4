5fb47d3fc6f2e7a56d8c49c78593dd005273532c
Harrison R. Lara <harrisonlara97@gmail.com>
2020-02-26 20:38:46 -0600

    add route methods to src

--- Commerce-Project/client/src/api/index.tsx
+++ Commerce-Project/client/src/api/index.tsx
@@ -4,11 +4,20 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api',
 })
 
+// users
 export const createUser = (payload: any) => api.post(`/user`, payload)
 export const getUser = (payload: any) => api.post(`/users`, payload)
 export const updateUser = (id: string, payload: any) => api.put(`/user/${id}`, payload)
 export const deleteUser = (id: string) => api.delete(`/user/${id}`)
 export const getUserById = (id: string) => api.get(`/user/${id}`)
+
+//accounts 
+export const createChecking = (payload: any) => api.post(`/user`, payload)
+export const getChecking = () => api.post(`/user`)
+export const createSavings = (payload: any) => api.post(`/user`, payload)
+export const getSavings = () => api.post(`/user`)
+export const createMoneyMarket = (payload: any) => api.post(`/user`, payload)
+export const getMoneyMarket = () => api.post(`/user`)
 
 const apis = {
   createUser,
@@ -16,6 +25,12 @@ const apis = {
   updateUser,
   deleteUser,
   getUserById,
+  createChecking,
+  getChecking,
+  createSavings,
+  getSavings,
+  createMoneyMarket,
+  getMoneyMarket
 }
 
 export default apis
