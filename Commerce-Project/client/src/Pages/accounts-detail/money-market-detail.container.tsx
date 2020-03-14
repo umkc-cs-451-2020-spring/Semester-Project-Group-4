bb4f77020b0c2ba856515f4deb227fe2c1880c82
Harrison R. Lara <harrisonlara97@gmail.com>
2020-03-14 13:58:04 -0500

    create grid and sorting for MM detail page

--- Commerce-Project/client/src/Pages/accounts-detail/money-market-detail.container.tsx
+++ Commerce-Project/client/src/Pages/accounts-detail/money-market-detail.container.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Divider, Paper, Typography } from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/styles';
+import { Grid, Table, TableHeaderRow, TableColumnResizing } from '@devexpress/dx-react-grid-material-ui';
 import store from "store";
 import apis from '../../api';
+import { IntegratedSorting, SortingState } from '@devexpress/dx-react-grid';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -17,12 +19,31 @@ const useStyles = makeStyles(() =>
   })
 );
 
+export interface accountDetails {
+  accountNumber: number,
+  processDate: string,
+  createdAt: string,
+  actionType: string,
+  amount: number,
+  description: string
+}
+
 const MoneyMarketDetail = () => {
   // styles
   const { paper, divider } = useStyles()
 
   // Hooks
   const [moneyMarket, setMoneyMarket] = React.useState(0);
+  const [rows, setRows] = React.useState([]);
+  const [columnWidths, setColumnWidths] = React.useState<any>([
+    { columnName: 'accountNumber', width: 180 },
+    { columnName: 'processDate', width: 180 },
+    { columnName: 'createdAt', width: 240 },
+    { columnName: 'actionType', width: 180 },
+    { columnName: 'amount', width: 180 },
+    { columnName: 'description', width: 240 }
+  ]);
+  const [sorting, setSorting] = React.useState<any>([])
 
   const storage = store.get('username');
 
@@ -31,15 +52,45 @@ const MoneyMarketDetail = () => {
     setMoneyMarket(money.data.data[0].amount);
   }
 
+  const getRows = async () => {
+    let moneyRows = await apis.getMoneyMarket(storage);
+    const row = moneyRows.data.data;
+    return setRows(row);
+  }
+
   React.useEffect(() => {
     getBalances();
-  })
+    getRows();
+  }, [])
+
+  const columns = [
+    { name: 'accountNumber', title: 'Account Number' },
+    { name: 'processDate', title: 'Process Date' },
+    { name: 'createdAt', title: 'Created At' },
+    { name: 'actionType', title: 'Action' },
+    { name: 'amount', title: 'Amount' },
+    { name: 'description', title: 'Description' }
+  ];
 
   return (
     <Paper elevation={0} className={paper}>
-      <Typography variant='h5'>Money Market Account</Typography>
+      <Typography variant='h5'>Money Market Account -- {moneyMarket}</Typography>
       <Divider className={divider} />
-      {moneyMarket}
+      <Paper style={{ position: 'relative' }}>
+        <Grid rows={rows} columns={columns}>
+          <SortingState
+            sorting={sorting}
+            onSortingChange={setSorting}
+          />
+          <IntegratedSorting />
+          <Table />
+          <TableColumnResizing
+            columnWidths={columnWidths}
+            onColumnWidthsChange={setColumnWidths}
+          />
+          <TableHeaderRow showSortingControls />
+        </Grid>
+      </Paper>
     </Paper >
   )
 }
