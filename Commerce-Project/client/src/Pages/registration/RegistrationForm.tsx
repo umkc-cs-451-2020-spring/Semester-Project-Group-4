c486ef3dd1ba4fe74f30b76d31ee5668744ee310
Harrison R. Lara <harrisonlara97@gmail.com>
2020-04-19 11:53:33 -0500

    update login and registration with paper

--- Commerce-Project/client/src/Pages/registration/RegistrationForm.tsx
+++ Commerce-Project/client/src/Pages/registration/RegistrationForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField, Button, FormGroup, Grid, Typography } from '@material-ui/core'
+import { TextField, Button, FormGroup, Grid, Typography, Paper } from '@material-ui/core'
 import { useFormik } from 'formik';
 import { makeStyles, createStyles } from '@material-ui/styles';
 import apis from '../../api';
@@ -8,6 +8,11 @@ import { useLocalStorageSetState } from '../../utils/local-storage';
 
 const useStyles = makeStyles(() =>
   createStyles({
+    paper: {
+      padding: '1rem',
+      height: '100%',
+      margin: '5rem 1rem',
+    },
     textFieldMargin: {
       margin: '.5rem'
     },
@@ -19,8 +24,7 @@ const useStyles = makeStyles(() =>
       width: '100%'
     },
     title: {
-      marginTop: '3.125rem',
-      marginBottom: '1rem',
+      marginBottom: '1rem'
     },
     font: {
       fontSize: 0
@@ -69,7 +73,7 @@ export const validate = (values: any) => {
 };
 
 const RegistrationForm = () => {
-  const { textFieldMargin, buttonMargin, gridMargin, title, font } = useStyles();
+  const { textFieldMargin, buttonMargin, gridMargin, title, font, paper } = useStyles();
   const [next, setNext] = React.useState(false)
   const [name, setName] = useLocalStorageSetState('', 'username')
 
@@ -98,100 +102,102 @@ const RegistrationForm = () => {
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <Grid container spacing={8} className={gridMargin}>
-        <Grid item xs={4} />
-        <Grid item xs={4}>
-          <FormGroup>
-            <Typography className={title} align='center' variant="h5">Create Account</Typography>
-            <TextField
-              className={textFieldMargin}
-              label="First Name"
-              id="firstName"
-              name="firstName"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.firstName}
-              helperText={formik.errors.firstName && formik.touched.firstName ? formik.errors.firstName : null}
-              error={formik.touched.firstName && formik.errors.firstName ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Last Name"
-              id="lastName"
-              name="lastName"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.lastName}
-              helperText={formik.errors.lastName && formik.touched.lastName ? formik.errors.lastName : null}
-              error={formik.touched.lastName && formik.errors.lastName ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Username"
-              id="username"
-              name="username"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.username}
-              helperText={formik.errors.username && formik.touched.username ? formik.errors.username : null}
-              error={formik.touched.username && formik.errors.username ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Email"
-              id="email"
-              name="email"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              helperText={formik.errors.email && formik.touched.email ? formik.errors.email : null}
-              error={formik.touched.email && formik.errors.email ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Phone Number"
-              id="phone"
-              name="phone"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.phone}
-              helperText={formik.errors.phone && formik.touched.phone ? formik.errors.phone : null}
-              error={formik.touched.phone && formik.errors.phone ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Password"
-              id="password"
-              name="password"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              helperText={formik.errors.password && formik.touched.password ? formik.errors.password : null}
-              error={formik.touched.password && formik.errors.password ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <TextField
-              className={textFieldMargin}
-              label="Confirm Password"
-              id="confirmPassword"
-              name="confirmPassword"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.confirmPassword}
-              helperText={formik.errors.confirmPassword && formik.touched.confirmPassword ? formik.errors.confirmPassword : null}
-              error={formik.touched.confirmPassword && formik.errors.confirmPassword ? true : false}
-              onBlur={formik.handleBlur}
-            />
-            <Button className={buttonMargin} disabled={formik.isSubmitting} type="submit" variant="contained" color="primary" onClick={formik.submitForm}>Register</Button>
-          </FormGroup>
+      <Paper elevation={0} className={paper}>
+        <Grid container spacing={8} className={gridMargin}>
+          <Grid item xs={4} />
+          <Grid item xs={4}>
+            <FormGroup>
+              <Typography className={title} align='center' variant="h5">Create Account</Typography>
+              <TextField
+                className={textFieldMargin}
+                label="First Name"
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+                helperText={formik.errors.firstName && formik.touched.firstName ? formik.errors.firstName : null}
+                error={formik.touched.firstName && formik.errors.firstName ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Last Name"
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+                helperText={formik.errors.lastName && formik.touched.lastName ? formik.errors.lastName : null}
+                error={formik.touched.lastName && formik.errors.lastName ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Username"
+                id="username"
+                name="username"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+                helperText={formik.errors.username && formik.touched.username ? formik.errors.username : null}
+                error={formik.touched.username && formik.errors.username ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Email"
+                id="email"
+                name="email"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                helperText={formik.errors.email && formik.touched.email ? formik.errors.email : null}
+                error={formik.touched.email && formik.errors.email ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Phone Number"
+                id="phone"
+                name="phone"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.phone}
+                helperText={formik.errors.phone && formik.touched.phone ? formik.errors.phone : null}
+                error={formik.touched.phone && formik.errors.phone ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Password"
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                helperText={formik.errors.password && formik.touched.password ? formik.errors.password : null}
+                error={formik.touched.password && formik.errors.password ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <TextField
+                className={textFieldMargin}
+                label="Confirm Password"
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.confirmPassword}
+                helperText={formik.errors.confirmPassword && formik.touched.confirmPassword ? formik.errors.confirmPassword : null}
+                error={formik.touched.confirmPassword && formik.errors.confirmPassword ? true : false}
+                onBlur={formik.handleBlur}
+              />
+              <Button className={buttonMargin} disabled={formik.isSubmitting} type="submit" variant="contained" color="primary" onClick={formik.submitForm}>Register</Button>
+            </FormGroup>
+          </Grid>
+          <Typography className={font}>{name}</Typography>
         </Grid>
-        <Typography className={font}>{name}</Typography>
-      </Grid>
+      </Paper>
       {next ? <Redirect to="/dashboard" /> : false}
     </form>
   )
